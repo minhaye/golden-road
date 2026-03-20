1b7ac410b97dd59e6c7f2fc6d1924abd21db996c
minh <minhtrinhvan4405@gmail.com>
2026-03-20 07:57:34 +0700

    them tinh nang ban dan theo vi tri con tro chuot

---
+++ src/goldenroad/entity/Bullet.java
@@ -0,0 +1,57 @@
+package goldenroad.entity;
+
+import java.awt.Color;
+import java.awt.Rectangle;
+
+public class Bullet {
+    private double x;
+    private double y;
+
+    private final double velocityX;
+    private final double velocityY;
+
+    private final int diameter;
+    private final Color color;
+
+    public Bullet(double x, double y, double directionX, double directionY, double speed, int diameter, Color color) {
+        this.x = x;
+        this.y = y;
+
+        double length = Math.hypot(directionX, directionY);
+        if (length == 0) {
+            directionX = 1;
+            directionY = 0;
+            length = 1;
+        }
+
+        this.velocityX = (directionX / length) * speed;
+        this.velocityY = (directionY / length) * speed;
+        this.diameter = diameter;
+        this.color = color;
+    }
+
+    public void update() {
+        x += velocityX;
+        y += velocityY;
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle((int) Math.round(x), (int) Math.round(y), diameter, diameter);
+    }
+
+    public int getRenderX() {
+        return (int) Math.round(x);
+    }
+
+    public int getRenderY() {
+        return (int) Math.round(y);
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
